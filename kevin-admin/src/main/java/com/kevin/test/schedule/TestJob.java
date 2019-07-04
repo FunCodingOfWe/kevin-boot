edf9b8ce63db44142cf1df2fbc21710f9b13bd05
runzhenghengbin <923604825@qq.com>
2019-07-04 23:11:40 +0800

    增加多个配置文件配置并测试成功

--- kevin-admin/src/main/java/com/kevin/test/schedule/TestJob.java
+++ kevin-admin/src/main/java/com/kevin/test/schedule/TestJob.java
@@ -1,6 +1,7 @@
 package com.kevin.test.schedule;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -8,8 +9,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class TestJob {
 
+    @Value("${name}")
+    private  String name;
     @Scheduled(fixedDelay = 5000, initialDelay = 2000)
     private void execute() {
-        log.info(">>>>>>>>>>>> Scheduled is run !");
+        log.info(">>>>>>>>>>>> Scheduled is run !"+name);
     }
 }
